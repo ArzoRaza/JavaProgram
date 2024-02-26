60cc85ce089f9d564192d36d191c4c33d2e6c9c4
Arzo Raza <142464887+ArzoRaza@users.noreply.github.com>
2024-02-26 22:50:47 +0530

    polymorphism in java language

---
+++ polytest.java
@@ -0,0 +1,12 @@
+package oop_encapsulation;
+
+public class polytest {
+
+        public void print (int a){
+            System.out.println("hello" + a);
+        }
+        public void print (float a){
+            System.out.println("to kaise hai app " + a);
+        }
+
+}
